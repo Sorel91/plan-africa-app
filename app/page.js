079bb0da5e19301546c96b6cb2f15ca0f9a8d24a
Sorel91 <51292520+Sorel91@users.noreply.github.com>
2026-03-25 20:34:23 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { supabase } from "../lib/supabase";
+import { useState } from "react";
 
 export default function Home() {
   const [fullName, setFullName] = useState("");
