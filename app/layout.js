6e9f6561a3ecc6023cd1917861e58275a8562e33
Sorel91 <beydi.sangare@gmail.com>
2026-03-29 17:48:23 +0200

    update Google Tag Manager and Google Ads tracking scripts in layout

--- app/layout.js
+++ app/layout.js
@@ -11,33 +11,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className="flex min-h-screen flex-col bg-slate-50 text-slate-900">
-        <Script id="gtm-base" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5MHQQ9Z6');
-          `}
-        </Script>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5MHQQ9Z6"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-417498809"
+          src="https://www.googletagmanager.com/gtag/js?id=G-100ED1S6YR"
           strategy="afterInteractive"
         />
-        <Script id="google-ads-gtag-init" strategy="afterInteractive">
+        <Script id="ga4-gtag-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-417498809');
+            gtag('config', 'G-100ED1S6YR');
           `}
         </Script>
         <header className="sticky top-0 z-50 border-b border-slate-200 bg-white/90 backdrop-blur">
